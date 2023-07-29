439386b4ac88fb77ddb6e25d1fb48515ba2191da
tylergarcia <tyleragarcia@gmail.com>
2023-07-28 23:18:33 -0500

    Create mas.sh

---
+++ mas.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# check if mas is installed
+
+echo "Installing RadarScope..."
+mas install 288419283
+
+echo "Installing Magnet..."
+mas install 441258766
+
+echo "Installing Xcode..."
+mas install 497799835
